5e418090317f7a55f389a963514777b73b355d0f
WanWen-Lai <87063896+WanWen-Lai@users.noreply.github.com>
2021-07-07 23:25:00 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+﻿function initial() {
+	Vue.component('main-menu', {
+		template: `<div>
+					<ul>
+						<li :class="{ active: type === '1' }">個人資料</li>
+						<li :class="{ active: type === '2' }">作品</li>
+					</ul>
+				</div>`,
+		props: ['type'],
+	})
+	new Vue({
+		el: '#main',
+		data: {
+			message: '主要語言為C#、JQ',
+			Name: '123',
+			NameEn: 'Wan Wen,Lai'
+		}
+	});
+}
