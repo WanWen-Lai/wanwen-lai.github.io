7afa26d4b84adf52714b000851e3f5fc9f1c26c5
WanWen-Lai <87063896+WanWen-Lai@users.noreply.github.com>
2021-07-12 00:49:42 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 ﻿function initial() {
 	Vue.component('main-menu', {
-		template: `<div>
+		template: `<div id="menu">
 					<ul>
-						<li :class="{ active: type === '1' }">個人資料</li>
-						<li :class="{ active: type === '2' }">作品</li>
+						<li :class="{ active: (type === '1' || IsActive) }" @mouseover="IsActive = (type === '1' ? false : true)"  @mouseout="IsActive = false"><a href="/">個人資料</a></li>
+						<li :class="{ active: (type === '2' || IsActive) }" @mouseover="IsActive = (type === '2' ? false : true)"  @mouseout="IsActive = false"><a href="works.html">作品</a></li>
 					</ul>
 				</div>`,
 		props: ['type'],
+		data: function () {
+			return {
+				IsActive: false
+			}
+		}
 	})
 	new Vue({
 		el: '#main',
 		data: {
-			message: '主要語言為C#、JQ',
-			Name: '123',
-			NameEn: 'Wan Wen,Lai'
+			Name: '賴婉雯',
+			NameEn: 'Wan Wen,Lai',
+			location: '新北 • 臺灣',
+			job: '網頁工程師'
 		}
 	});
+
+	new Vue({
+		el: '.goTop',
+		data: {
+			scrollNum: 0, //滾動距離
+			isTop: false, //是否顯示回到頂部按鈕
+		},
+		mounted: function () {
+			window.addEventListener("scroll", () => {
+				let top =
+					document.documentElement.scrollTop ||
+					document.body.scrollTop ||
+					window.pageYOffset;
+				this.scrollNum = top;
+				if (top >= 100) {
+					this.isTop = true;
+				} else {
+					this.isTop = false;
+				}
+			});
+		},
+		methods: {
+			goTop: function () {
+				document.documentElement.scrollTop = 0;
+			}
+		}
+	});
+
 }
